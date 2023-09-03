13ccd4ce43d96eb883c7893dced45f360f2bd091
tchi-sama <tchisama41@gmail.com>
2023-09-02 22:41:34 -0700

    some new

--- components/global/Feed.tsx
+++ components/global/Feed.tsx
@@ -1,22 +1,34 @@
 "use client"
-import React from 'react'
+import React, { useMemo } from 'react'
 import FeedCard from "../global/FeedCard"
 import StoreCard from './StoreCard'
 import { useSelector } from 'react-redux';
 import { Store } from '@/types/storeType';
 import { useStores } from '@/app/store';
-
-
+import axios from "axios"
 
 
 export default function Feed() {
-    const {stores}  = useStores()
+    const {stores,setStore}  = useStores()
+    useMemo(() => {
+        const getData = async ()=>{
+            try {
+                const response = await axios.post("http://localhost:8080/get-stores")
+                console.log(response.data)
+                setStore(response.data.data)
+            }catch(err){
+                console.log(err)
+            }
+        }
+        getData()
+    }, [])
+    
   return (
-    <div className='mt-8 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-3'>
+    <div className='py-8 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-3'>
       {
         stores.map((store,id)=>{
           return(
-            <StoreCard store={store}/>
+            <StoreCard key={id} store={store}/>
           )
         })
       }
